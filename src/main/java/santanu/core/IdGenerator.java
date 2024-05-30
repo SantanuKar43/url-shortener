77bbd60b96a947ae02f4178877bb1c32f1946606
SantanuKar43 <kar.santanu43@gmail.com>
2024-05-30 10:15:02 +0530

    add validation

--- src/main/java/santanu/core/IdGenerator.java
+++ src/main/java/santanu/core/IdGenerator.java
@@ -8,15 +8,22 @@ import java.time.temporal.ChronoField;
 
 @Singleton
 public class IdGenerator {
+
+    private static final int ID_LENGTH = 8;
+
     public String generate(String url) {
-        return DigestUtils.sha1Hex(url).substring(0, 8);
+        return DigestUtils.sha1Hex(url).substring(0, ID_LENGTH);
     }
 
     public String generateWithBuffer(String url) {
-        return DigestUtils.sha1Hex(url.concat(getBuffer())).substring(0, 8);
+        return DigestUtils.sha1Hex(url.concat(getBuffer())).substring(0, ID_LENGTH);
     }
 
     private static String getBuffer() {
         return String.valueOf(LocalDateTime.now().get(ChronoField.MICRO_OF_SECOND));
+    }
+
+    public boolean isValid(String id) {
+        return id.length() == ID_LENGTH;
     }
 }
--- src/main/java/santanu/core/service/RedirectionService.java
+++ src/main/java/santanu/core/service/RedirectionService.java
@@ -2,6 +2,7 @@ package santanu.core.service;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import santanu.core.IdGenerator;
 import santanu.core.entity.ShortUrl;
 import santanu.core.storage.UrlStore;
 
@@ -10,13 +11,16 @@ import java.util.Optional;
 @Singleton
 public class RedirectionService {
     private final UrlStore urlStore;
+    private final IdGenerator idGenerator;
 
     @Inject
-    public RedirectionService(UrlStore urlStore) {
+    public RedirectionService(UrlStore urlStore, IdGenerator idGenerator) {
         this.urlStore = urlStore;
+        this.idGenerator = idGenerator;
     }
 
     public Optional<ShortUrl> get(String id) {
+        if (!idGenerator.isValid(id)) return Optional.empty();
         return urlStore.get(id);
     }
 }
